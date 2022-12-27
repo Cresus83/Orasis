f9fcd64dbfdfe039ca62cfa73619286e2d2b4f15
Cresus83 <alan.thob@hotmail.fr>
2022-12-27 19:10:43 +0100

    update informations

--- informations.php
+++ informations.php
@@ -13,11 +13,11 @@
 <main id="main">
 
     <section>
-        <div class="title">
-            <span>informations</span>
-            <h2>informations</h2>
-        </div>
         <div class="container">
+            <div class="title">
+                <span>informations</span>
+                <h2>informations</h2>
+            </div>
             <div class="title-section">
                 <h1>hébergement & restauration</h1>
             </div>
@@ -37,7 +37,7 @@
                 </div>
 
                 <div class="bloc-text">
-                    <p>Le site peut accueillir jusqu’à 100 participants avec la configuration suivante: 70 chambres
+                    <p>Le site peut accueillir jusqu’à 100 participants avec la configuration suivante : 70 chambres
                         simples et 15 chambres doubles (photo ci-dessous).</p>
                 </div>
 
@@ -45,12 +45,12 @@
 
                 <div class="bloc-text">
                     <ul>
-                        <p>Le village club propose également divers services et équipements :<br><br>
+                        Le village club propose également divers services et équipements :<br><br>
                         <li>
-                            Bar avec terrasse de 600 m2 et solarium.
+                            Bar avec terrasse de 600 m² et solarium.
                         </li>
                         <li>
-                            Espace aquatique de 1200 m2 chauffé d’avril à octobre avec couloir de nage, bain à remous et
+                            Espace aquatique de 1200 m² chauffé d’avril à octobre avec couloir de nage, bain à remous et
                             chemin
                             massant.
                         </li>
@@ -58,18 +58,84 @@
                             Parking accessible aux autocars Mini-golf, volley, badminton, tennis, tir à l’arc, jeu de
                             boules.
                         </li>
-                        </p>
                     </ul>
-
-
                 </div>
 
+                <div class="doublesquare">
+                    <div class="conf-img3">
+                        <img alt="image du centre Miléade" src="assets/img/conf-img3.png">
+                    </div>
 
+                    <div class="conf-text2">
+                        <ul>
+                            Les frais d’inscription incluent un service de restauration couvrant toute la période de la
+                            conférence pour tous les participants :<br><br>
+                            <li>
+                                4 petits déjeuners.
+                            </li>
+                            <li>
+                                8 repas (4 diners et 4 déjeuners). Boisson incluse aux repas.
+                            </li>
+                            <li>
+                                6 pauses café : café, thé, jus de fruits, viennoiseries.
+                            </li>
+                        </ul>
+                        <a href="#">
+                            <button class="btnCTA">comment s'inscrire ? <i class="fa fa-solid fa-chevron-right"></i>
+                            </button>
+                        </a>
+                    </div>
+                </div>
             </div>
         </div>
         <!-- container -->
     </section>
 
+    <section>
+        <div class="container">
+            <div class="title-section">
+                <h1>plan d'accès</h1>
+            </div>
+            <div class="conference">
+                <div class="plan-acces">
+                    <div class="gare">
+                        <h2>depuis la gare de toulon</h2>
+                        <p>
+                            Il est possible de prendre la ligne de bus 39 (direction Lycée Golf Hôtel) à la gare SNCF de
+                            Toulon (l'arrêt Vauban à 400m de la gare SNCF) qui vous amène à l'arrêt Valérane,
+                            Carqueiranne.<br><br>
+                            <a target="_blank"
+                               href="https://www.reseaumistral.com/fr/itineraires/4/TripPlanner/index?KeywordDep=Gare%20SNCF%20de%20Toulon%20-%20TOULON&PointDep=205000005_1&KeywordArr=600%2C%20AVENUE%20DE%20LA%20VALERANE%20-%20CARQUEIRANNE&NumArr=600&PointArr=707_3&Date=08%2F09%2F2022&TypeDate=68&Hour=17&Minute=20&DurationVia=30&Algorithm=Fastest&TypeTrip=PlanTrip&ListModes=Bus%7CBoat%7CSchool%7CTod&IgnoreDisruptions=False&WalkDistance=1000&WalkSpeed=4&CarDistance=25&CarLeave=0&BikeDistance=10&BikeLeave=0&BikeSpeed=10&Accessibility=0&IsoBefore=True&IsSeveralNetworks=False&Submit=False&ExpandCriteria=0&Length=11&_=1662649649775">Information
+                                du trajet entre Toulon et Carqueiranne par bus 39</a><br><br>
+                            Téléchargez gratuitement l'application Réseau Mistral pour plus de facilité !<br><br>
+                        </p>
+                        <div class="app-download">
+                            <a target="_blank"
+                               href="https://play.google.com/store/apps/details?id=fr.cityway.android.rmtt&hl=fr&pli=1"><img
+                                        alt="Google Play App Download"
+                                        src="assets/img/google-play-app-download.png"></a>
+                            <a target="_blank" href="https://apps.apple.com/fr/app/reseau-mistral/id429896529"><img
+                                        alt="App Store App Download" src="assets/img/app-store-app-download.png"></a>
+                        </div>
+
+                    </div>
+                    <div class="aeroport">
+                        <h2>depuis l'aeroport de toulon-hyères</h2>
+                        <p>
+                            <span>En bus :</span><br>
+                            Prenez la ligne de bus 63 (direction Hyères centre) qui amène à l'arrêt Jean Jaurès, ensuite
+                            prenez la ligne de bus 39 (direction Gare routière de Toulon) pour aller à l'arrêt
+                            Valérane.<br><br>
+
+                            <span>En taxi:</span><br>
+
+                            Il est aussi possible de prendre un taxi entre l'aéroport Toulon Hyères et le centre
+                            vacanciel Miléade (9km de trajet).
+                        </p>
+                    </div>
+                </div>
+
+            </div>
 </main>
 <!-- Footer Import -->
 <?php include "assets/includes/footer.php" ?>
