4169fd0827e7100ffca85e465eae6b7652b2736d
Cresus83 <alan.thob@hotmail.fr>
2022-12-27 13:37:07 +0100

    update informations

--- informations.php
+++ informations.php
@@ -18,12 +18,54 @@
             <h2>informations</h2>
         </div>
         <div class="container">
+            <div class="title-section">
+                <h1>hébergement & restauration</h1>
+            </div>
+            <div class="conference">
+                <div class="doublesquare">
+                    <div class="conf-text">
+                        <p>La conférence et les participants seront logés à l’établissement Miléade idéalement placé à
+                            Carqueiranne qui donne une vue sur mer.<br><br>
+                            Nous proposons une formule de pension complète incluse dans les prix d’inscription et de
+                            l’hébergement.<br><br>
+
+                            Les photos suivantes présentent les locaux et l’emplacement de cet établissement.</p>
+                    </div>
+                    <div class="conf-img">
+                        <img alt="image du centre Miléade" src="assets/img/conf-img1.png">
+                    </div>
+                </div>
+
+                <div class="bloc-text">
+                    <p>Le site peut accueillir jusqu’à 100 participants avec la configuration suivante: 70 chambres
+                        simples et 15 chambres doubles (photo ci-dessous).</p>
+                </div>
+
+                <img alt="image du centre Miléade" src="assets/img/conf-img2.png">
+
+                <div class="bloc-text">
+                    <ul>
+                        <p>Le village club propose également divers services et équipements :<br><br>
+                        <li>
+                            Bar avec terrasse de 600 m2 et solarium.
+                        </li>
+                        <li>
+                            Espace aquatique de 1200 m2 chauffé d’avril à octobre avec couloir de nage, bain à remous et
+                            chemin
+                            massant.
+                        </li>
+                        <li>
+                            Parking accessible aux autocars Mini-golf, volley, badminton, tennis, tir à l’arc, jeu de
+                            boules.
+                        </li>
+                        </p>
+                    </ul>
 
 
+                </div>
 
 
-
-
+            </div>
         </div>
         <!-- container -->
     </section>
