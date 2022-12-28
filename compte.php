1818b4597d4cc5d1eaa002c1a2c9cd8e2341850a
Karmaa83 <97305952+Karmaa83@users.noreply.github.com>
2022-12-28 17:32:31 +0100

    Page espace perso complet

--- compte.php
+++ compte.php
@@ -52,31 +52,87 @@
                     </form>
                     <!-- Section "MES DEPOTS" -->
                     <div class="deposit">
-                        <h3>Mes dépôts<h3>
-                                <table class="table_deposit">
-                                    <tr>
-                                        <th>Titre du dépôt</th>
-                                        <th>Date du dépôt</th>
-                                        <th>Statut du dépôt</th>
+                        <h3>Mes dépôts</h3>
+                        <table class="table_deposit">
+                            <tr>
+                                <th>Titre du dépôt</th>
+                                <th>Date du dépôt</th>
+                                <th>Statut du dépôt</th>
 
-                                    </tr>
-                                    <tr class="line">
-                                        <td data-th="Titre du dépôt">blabla.pdf</td>
-                                        <td data-th="Date du dépôt">JJ/MM/AAAA</td>
-                                        <td class="checking" data-th="Statut du dépôt">En cours de traîtement</td>
-                                    </tr>
-                                    <tr class="line">
-                                        <td data-th="Titre du dépôt">blabla.pdf</td>
-                                        <td data-th="Date du dépôt">JJ/MM/AAAA</td>
-                                        <td class="refused" data-th="Statut du dépôt">Refusé</td>
-                                    </tr>
-                                    <tr class="line">
-                                        <td data-th="Titre du dépôt">blabla.pdf</td>
-                                        <td data-th="Date du dépôt">JJ/MM/AAAA</td>
-                                        <td class="validate" data-th="Statut du dépôt">Validé</td>
-                                    </tr>
+                            </tr>
+                            <tr class="line">
+                                <td data-th="Titre du dépôt">blabla.pdf</td>
+                                <td data-th="Date du dépôt">JJ/MM/AAAA</td>
+                                <td class="checking" data-th="Statut du dépôt">En cours</td>
+                            </tr>
+                            <tr class="line">
+                                <td data-th="Titre du dépôt">blabla.pdf</td>
+                                <td data-th="Date du dépôt">JJ/MM/AAAA</td>
+                                <td class="refused" data-th="Statut du dépôt">Refusé</td>
+                            </tr>
+                            <tr class="line">
+                                <td data-th="Titre du dépôt">blabla.pdf</td>
+                                <td data-th="Date du dépôt">JJ/MM/AAAA</td>
+                                <td class="validate" data-th="Statut du dépôt">Validé</td>
+                            </tr>
 
-                                </table>
+                        </table>
+                    </div>
+
+                    <!-- Section "MON INSCRIPTION" -->
+                    <div class="inscription" style="display:none">
+                        <h3>Mon inscription</h3>
+                        <div class="inscription_card">
+                            <h4>Statut de l’inscription en tant que participant/auteur</h4>
+                            <div class="double">
+                                <div class="progress">
+                                    <ul class="steps steps--vertical">
+                                        <li class="steps__item steps__item--complete">
+                                            <div class="steps__label"> Renseignement des informations</div>
+                                            <div class="steps__space"></div>
+                                        </li>
+                                        <li class="steps__item steps__item--current">
+                                            <div class="steps__label"> En attente du virement des frais
+                                                d’inscription</div>
+                                            <div class="steps__space"></div>
+                                        </li>
+                                        <li class="steps__item">
+                                            <div class="steps__label"> Inscription finalisé</div>
+                                            <div class="steps__space"></div>
+                                        </li>
+                                    </ul>
+                                    <div class="legend">
+                                        <div class="finalised">
+                                            <p>Étape validée</p>
+                                        </div>
+                                        <div class="currently">
+                                            <p>Étape actuelle</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="text">
+                                    <p>Une fois que vous avez effectué le virement bancaire, veuillez nous en
+                                        informer et nous communiquer la référence du virement via
+                                        orasis23@scienceconf.com. Nous vous répondrons une fois que nous aurons
+                                        reçu les frais d’inscription.
+                                        <br><br>
+                                        <span class="strong">Instructions de paiement</span><br>
+                                        Veuillez transférer les frais sur le compte suivant (USD) :
+                                        <br><br>
+                                    <ul>
+                                        <li><span class="strong">Bénéficiaire:</span> Laboratoire d'Informatique
+                                            et
+                                            de Systèmes (LIS, UMR 7020)</li>
+                                        <li><span class="strong">Description:</span> ORASIS'23- NOM et Prénom du
+                                            participant- PaperID</li>
+                                    </ul>
+                                    </p>
+
+                                </div>
+                            </div>
+                        </div>
+                        <p class="remarque">Pour valider votre inscription, n’oubliez pas d’effectuez le virement comme
+                            expliqué <a href="inscription.php">ici</a>.</p>
                     </div>
                 </div>
 
