89bdb138769818f5f16ae6d51a69c75c10756bd5
Karmaa83 <97305952+Karmaa83@users.noreply.github.com>
2022-12-29 14:04:30 +0100

    Contact form

--- contact.php
+++ contact.php
@@ -7,7 +7,9 @@
 
 <body>
     <!-- Navbar Import -->
-    <?php include "assets/includes/navbar.php" ?>
+    <?php include "assets/includes/navbar.php";
+    include "mailSent.php";
+    ?>
 
     <!-- Section LOGIN -->
     <section>
@@ -17,20 +19,20 @@
                     <img src="assets/img/gif/message.gif" alt="GIF Mail">
                     <h3>Contact</h3>
                 </div>
-                <form>
+                <form method="post" action="">
 
                     <div class="input_contact">
-                        <input class="input_text mr" type="text" name="nom" placeholder="Nom">
+                        <input class="input_text mr" type="text" name="nom" placeholder="Nom" required>
 
-                        <input class="input_text" type="password" name="prenom" placeholder="Prénom">
+                        <input class="input_text" type="text" name="prenom" placeholder="Prénom" required>
                     </div>
 
-                    <input class="input_text alone" type="text" name="email" placeholder="Adresse email">
+                    <input class="input_text alone" type="text" name="email" placeholder="Adresse email" required>
 
-                    <textarea class="message" name="msg" placeholder="Votre message"></textarea>
+                    <textarea class="message" name="msg" placeholder="Votre message" required></textarea>
 
                     <input type="submit" name="valider" value="Envoyer">
-
+                    <?php echo $messageValid; ?>
                 </form>
 
             </div>
--- mailSent.php
+++ mailSent.php
@@ -1,20 +1,24 @@
 <?php
 // Récuperation des valeurs indiquer dans le formulaire
-    $nom = $_POST['nom'];
-    $prenom = $_POST['prenom'];
-    $email = $_POST['email'];
-    $immadh = $_POST['immatri'];
-    $messagecont = $_POST['message'];
+    @$nom = $_POST['nom'];
+    @$prenom = $_POST['prenom'];
+    @$email = $_POST['email'];
+    @$messagecont = $_POST['msg'];
+    @$validerBtn = $_POST['valider'];
+    @$messageValid = "";
+    @$nomprenom = $nom . ' ' . $prenom;
 
-
+    
+        
+    
 // lance les classes de PHPMailer
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
+if (isset($validerBtn)) {
 // chemin du dossier PHPMailer % fichier d'envoi du mail
 require 'PHPMailer/src/Exception.php';
 require 'PHPMailer/src/PHPMailer.php';
 require 'PHPMailer/src/SMTP.php';
-
 
 // passer true dans le constructeur active les exceptions dans PHPMailer
 $mail = new PHPMailer(true);
@@ -23,35 +27,36 @@ $mail->CharSet = "UTF-8";
 
 try {
     // Paramêtres de serveur
-    $mail->isSendmail();
+    // $mail->isSendmail();
+    $mail->isSMTP();
     $mail->Host = 'smtp.gmail.com';
     $mail->SMTPAuth = true;
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-    $mail->Port = 587;
+    $mail->SMTPSecure = "ssl";
+    $mail->Port = 465;
 	$mail->setLanguage('fr', '/optional/path/to/language/directory/');
 
-    $mail->Username = 'glideworld.contact@gmail.com'; // YOUR gmail email
-    $mail->Password = 'rayan83500'; // YOUR gmail password
+    $mail->Username = 'contact.orasis2023@gmail.com'; // YOUR gmail email
+    $mail->Password = 'hcvgxezomyxagpvw'; // YOUR gmail password
 
     // Expéditeur paramêtre
     $mail->setFrom($email);
-    $mail->addAddress($email);
-    $mail->addBCC("glideworld.contact@gmail.com");
+    $mail->addAddress("contact.orasis2023@gmail.com");
+    $mail->addReplyTo($email, $nomprenom);
 
     // Contenue de l'email
     $mail->IsHTML(true);
-    $mail->Subject = "Contact par BDD";
-    $mail->Body =  			"\!/ Ceci est une copie de votre formulaire \!/".
+    $mail->Subject = "[ORASIS 2023] Contact formulaire";
+    $mail->Body =  			"\!/ Ceci est un email envoyé depuis le formulaire de contact du site ORASIS 2023 \!/".
     						"<li>Message de $nom $prenom </li>".
-                            "<li>Immatriculé : $immadh </li>".
                             "<li>Le message: $messagecont</li>".
                             
 '<b>Merci de ne pas répondre à cet e-mail.</b>';
     $mail->send();
     // Affiche la page qui indiquent que l'email a bien été envoyée
-    header('Location: envoi_reussie.php');
+    $messageValid = "<p style='color:green;margin:2% auto;'>Le mail à bien été envoyé, merci de nous avoir contacté.</p>";
 } catch (Exception $e) {
-    header('Location: envoie_fail.php');
+    $messageValid = "<p style='color:red;margin:2% auto;'>Le mail n'a pas pu être envoyé, merci de revoir vos informations ou retentez plus tard.</p>";
+    //echo 'Caught exception: ',  $e->getMessage(), "\n";
 }
-
+}
 ?>
